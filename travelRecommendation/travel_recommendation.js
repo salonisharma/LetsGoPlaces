c26bc3593d51662049229041634350080a0e9f87
Saloni Sharma <mailsalonisharma@gmail.com>
2024-08-02 14:26:04 -0400

    travel site completed

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -1,43 +1,70 @@
-debugger;
+//debugger;
 const btnSearch = document.getElementById('btnSearch');
 btnSearch.addEventListener('click', searchlocation);
 function searchlocation() {
     const input = document.getElementById('searchText').value.toLowerCase();
-    //const resultDiv = document.getElementById('result');
-    //resultDiv.innerHTML = '';
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
 
     fetch('travel_recommendation_api.json')
       .then(response => response.json())
       .then(data => {
+        debugger;
         const countries = data.countries.find(item => item.name.toLowerCase() === input);
-        const temples = data.temples.find(item => item.name.toLowerCase() === input);
-        const beaches = data.beaches.find(item => item.name.toLowerCase() === input);
+        //const temples = data.temples.find(item => item.name.toLowerCase() === input);
+        //const beaches = data.beaches.find(item => item.name.toLowerCase() === input);
 
         if (countries) {
             console.log(countries);
-          /*const symptoms = countries.symptoms.join(', ');
-          const prevention = condition.prevention.join(', ');
-          const treatment = condition.treatment;
-
-          resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-          resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
-
-          resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
-          resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
-          resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;*/
-        } else if(temples) {
-            console.log(temples);
-        } else if(beaches){
-            console.log(beaches);
+            debugger;
+            for (let i = 0; i < countries.cities.length; i++) {
+               const cityname = countries.cities[i].name;
+               const imageUrl = countries.cities[i].imageUrl;
+               const desc = countries.cities[i].description;
+                resultDiv.innerHTML+=`<div id="${input}">`;
+               resultDiv.innerHTML += `<img src="${imageUrl}" alt="${cityname}">`;
+               resultDiv.innerHTML += `<h2 style="color:white">${cityname}</h2>`;
+               resultDiv.innerHTML += `<button class="search-button" id="visitButton">Visit</button>`;
+               resultDiv.innerHTML += `<p style="color:white">${desc}</p></div>`;
+              }
+        } else if(input=="temples") {
+            console.log(data.temples);
+            for (let i = 0; i < data.temples.length; i++) {
+                const cityname = data.temples[i].name;
+                const imageUrl = data.temples[i].imageUrl;
+                const desc = data.temples[i].description;
+                 resultDiv.innerHTML+=`<div id="${input}">`;
+                resultDiv.innerHTML += `<img src="${imageUrl}" alt="${cityname}">`;
+                resultDiv.innerHTML += `<h2 style="color:white">${cityname}</h2>`;
+                resultDiv.innerHTML += `<button class="search-button" id="visitButton">Visit</button>`;
+                resultDiv.innerHTML += `<p style="color:white">${desc}</p></div>`;
+               }
+        } else if(input=="beaches"){
+            console.log(data.beaches);
+            for (let i = 0; i < data.beaches.length; i++) {
+                const cityname = data.beaches[i].name;
+                const imageUrl = data.beaches[i].imageUrl;
+                const desc = data.beaches[i].description;
+                 resultDiv.innerHTML+=`<div id="${input}">`;
+                resultDiv.innerHTML += `<img src="${imageUrl}" alt="${cityname}">`;
+                resultDiv.innerHTML += `<h2 style="color:white">${cityname}</h2>`;
+                resultDiv.innerHTML += `<button class="search-button" id="visitButton">Visit</button>`;
+                resultDiv.innerHTML += `<p style="color:white">${desc}</p></div>`;
+               }
         }
         else {
             console.log("location not found");
-          //resultDiv.innerHTML = 'Condition not found.';
+            resultDiv.innerHTML += `<h1 style="color:white">Location Not Found</h1>`;
         }
       })
       .catch(error => {
         console.error('Error:', error);
-        //resultDiv.innerHTML = 'An error occurred while fetching data.';
+        resultDiv.innerHTML += `<h1 style="color:white">An error occurred while fetching location.</h1>`;
       });
   }
  
+  /*const visitButton = document.getElementById('visitButton');
+  visitButton.addEventListener('click', visitButton);
+function visitButton() {
+    location.replace("./travel_recommendation_dummyScreen.html");
+}*/
